209263c1f7115f8abffec757cfa39b471a12367c
Anrighe <assoonpc@gmail.com>
2022-12-14 23:59:35 +0100

    Solution debug

--- day08a/src/main.cpp
+++ day08a/src/main.cpp
@@ -73,24 +73,54 @@ void printGrid(int ** grid, int gridWidth, int gridHeight)
     }
 }
 
-bool isTreeOnEdge()
+bool isTreeOnEdge(int ** grid, int gridWidth, int gridHeight, int currentWidth, int currentHeight)
 {
+    if ((currentHeight == 0) || (currentHeight == gridHeight-1) || (currentWidth == 0) || (currentWidth == gridWidth-1))
+    {
+        //std::cout<<"grid["<<currentHeight<<"]["<<currentWidth<<"]: "<<grid[currentHeight][currentWidth]<<" is on the edge"<<std::endl;
+        return true;
+    }
+    else
+        return false;
+}
+bool isTreeVisibleNorth(int ** grid, int gridWidth, int gridHeight,  int currentHeight, int currentWidth)
+{
+    for (int i = 1; currentHeight-i >= 0; i++)
+    {
+        if (grid[currentHeight][currentWidth] <= grid[currentHeight-i][currentWidth])
+            return false;
+    }
     return true;
 }
-bool isTreeVisibleNorth()
+bool isTreeVisibleSouth(int ** grid, int gridWidth, int gridHeight,  int currentHeight, int currentWidth)
 {
+    for (int i = 1; currentHeight+i < gridHeight; i++)
+    {
+        if (grid[currentHeight][currentWidth] <= grid[currentHeight+i][currentWidth])
+            return false;
+    }
     return true;
 }
-bool isTreeVisibleSouth()
+bool isTreeVisibleEast(int ** grid, int gridWidth, int gridHeight,  int currentHeight, int currentWidth)
 {
+    for (int i = 1; currentWidth+i < gridWidth; i++)
+    {
+        //std::cout<<"Checking if ""grid["<<i<<"]["<<j<<"]: "<<grid[i][j]<<" is visible"<<std::endl;
+        if (grid[currentHeight][currentWidth] <= grid[currentHeight][currentWidth+i])
+            return false;
+    }
     return true;
 }
-bool isTreeVisibleEast()
+bool isTreeVisibleWest(int ** grid, int gridWidth, int gridHeight,  int currentHeight, int currentWidth)
 {
-    return true;
-}
-bool isTreeVisibleWest()
-{
+    for (int i = 1; currentWidth-i >= 0; i++)
+    {
+        if (grid[currentHeight][currentWidth] <= grid[currentHeight][currentWidth-i])
+        {
+            std::cout<<"WEST RETURNING FALSE"<<std::endl;
+            return false;
+        }
+    }
     return true;
 }
 
@@ -103,10 +133,30 @@ bool isTreeVisibleWest()
  * @return true if visible
  * @return false if not visible
  */
-bool isTreeVisibile()
+bool isTreeVisibile(int ** grid, int gridWidth, int gridHeight, int currentHeight, int currentWidth)
 {
+    if (isTreeVisibleNorth(grid, gridWidth, gridHeight, currentHeight, currentWidth) == true) 
+    {
+        std::cout<<"FROM NORTH"<<std::endl;
+        return true;
+    }
+    if (isTreeVisibleSouth(grid, gridWidth, gridHeight, currentHeight, currentWidth) == true)
+    {
+        std::cout<<"FROM SOUTH"<<std::endl;
+        return true;
+    }
+    if (isTreeVisibleEast(grid, gridWidth, gridHeight, currentHeight, currentWidth) == true)
+    {
+        std::cout<<"FROM EAST"<<std::endl;
+        return true;
+    }
+    if (isTreeVisibleWest(grid, gridWidth, gridHeight, currentHeight, currentWidth) == true)
+    {
+        std::cout<<"FROM WEST"<<std::endl;
+        return true;
+    }
 
-    return true;
+    return false;
 }
 
 /**
@@ -165,15 +215,51 @@ int visibleTreeCalculator(std::string pathToFile)
     int visibleTreeCounter = 0;
     int gridWidth = findGridWidth(pathToFile);
     int gridHeight = findGridHeight(pathToFile);
-    
-    int ** grid = generateGrid(gridWidth, gridHeight);
+    //std::cout<<gridWidth<<std::endl;
+    //std::cout<<gridHeight<<std::endl;
 
-    //grid[2][1] = 5; //DEBUG IT SEEMS TO WORK
+    int ** grid = generateGrid(gridWidth, gridHeight);
+    //grid[RIGA][COLONNA] = 5; //DEBUG IT SEEMS TO WORK
 
     grid = gridPopulator(grid, gridWidth, gridHeight, pathToFile);
-    printGrid(grid, gridWidth, gridHeight);
+    //printGrid(grid, gridWidth, gridHeight); //debug
 
-
+    for (int i = 0; i < gridHeight; ++i)
+    {
+        for (int j = 0; j < gridWidth; ++j)
+        {
+            
+            /*
+            if (i == 0 || j == 0 || i == gridHeight-1 || j == gridWidth-1)
+            {
+                std::cout<<"grid["<<i<<"]["<<j<<"]: "<<grid[i][j]<<" is on the edge"<<std::endl;
+                visibleTreeCounter++;
+            }*/
+            if (isTreeOnEdge(grid, gridWidth, gridHeight, j, i) == true)
+            {   
+                //std::cout<<"grid["<<j<<"]["<<i<<"]: "<<grid[j][i]<<" is on the edge"<<std::endl;
+                visibleTreeCounter++;
+                //std::cout<<grid[i][j]<<" is on the edge"<<std::endl;
+            }
+            else
+            {
+                if(isTreeVisibile(grid, gridWidth, gridHeight, i, j) == true)
+                {
+                    std::cout<<"grid["<<i<<"]["<<j<<"]: "<<grid[i][j]<<" is visible"<<std::endl<<std::endl;
+                    visibleTreeCounter++;
+                    //std::cin>>visibleTreeCounter;
+                }
+                else
+                    std::cout<<"grid["<<i<<"]["<<j<<"]: "<<grid[i][j]<<" isn't visible"<<std::endl<<std::endl;
+                /*
+                if (grid[i][j] > grid[i-1][j] && grid[i][j] > grid[i+1][j] && grid[i][j] > grid[i][j-1] && grid[i][j] > grid[i][j+1])
+                {
+                    std::cout<<"grid["<<i<<"]["<<j<<"]: "<<grid[i][j]<<" is visible"<<std::endl;
+                    visibleTreeCounter++;
+                }*/
+            }
+        }
+    }
 
     return visibleTreeCounter;
 }
