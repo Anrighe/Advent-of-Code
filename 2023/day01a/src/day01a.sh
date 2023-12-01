9e4f75c56db34bb5c247be855e80680176c17c41
Anrighe <assoonpc@gmail.com>
2023-12-01 11:35:57 +0100

    day01a solution

---
+++ 2023/day01a/src/day01a.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+input="input.txt"
+declare -i sum=0
+
+re='^[0-9]$'
+
+while IFS= read -r line || [ -n "$line" ]; do
+
+    found_first=false
+    found_second=false
+    value=''
+    for (( i=0; i<${#line}; i++)); do
+
+        # ${line:$i:1} expands to the substring starting at position $i of length 1
+        if [[ ${line:$i:1} =~ $re ]]; then 
+            value="$value${line:$i:1}"
+        fi
+
+    done
+
+    value="${value:0:1}${value: -1}" # Keeps only the first and the last digit of value
+    sum=$(($sum + $value))
+
+done < "$input"
+echo "The sum of all of the calibration values is: $sum"
+
+
+
