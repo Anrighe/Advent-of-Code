78747b0e3608e79ece242eb347668aa8eb7bdbf1
Anrighe <assoonpc@gmail.com>
2024-01-02 21:54:46 +0100

    Solution day09a

--- 2023/day09a/src/Solution.java
+++ 2023/day09a/src/Solution.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import javafx.util.*;
@@ -11,26 +13,70 @@ import javafx.util.*;
 public class Solution {
 
     /**
+     * Checks if all elements in the given sequence are zero.
+     *
+     * @param sequence the list of integers to check
+     * @return true if all elements are zero, false otherwise
+     */
+    private static boolean allElementsAreZero(List<Integer> sequence) {
+        for (int s : sequence) {
+            if (s != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int getNextValue(List<Integer> sequence) {
+        List<Integer> currentSequence = sequence;
+
+        List<List<Integer>> allSequences = new ArrayList<List<Integer>>();
+        allElementsAreZero(currentSequence);
+
+        int sum = 0;
+
+        while (!allElementsAreZero(currentSequence)) {
+
+            List<Integer> newSequance = new ArrayList<Integer>();
+            for (int i = 1; i < currentSequence.size(); i++) {
+                newSequance.add(currentSequence.get(i) - currentSequence.get(i - 1));
+
+            }
+            allSequences.add(newSequance);
+
+            currentSequence = newSequance;
+
+            sum += currentSequence.get(currentSequence.size() - 1);
+        }
+
+        // print allSequences
+        System.out.println("All sequences: ");
+        for (List<Integer> seq : allSequences) {
+            for (int s : seq) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+        return sequence.get(sequence.size() - 1) + sum;
+    }
+
+
+    /**
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
-        Long res = 0L;
+        int res = 0;
 
         try {
             File myObj = new File("input.txt");
             Scanner myReader = new Scanner(myObj);
 
-            String data = "";
-
-            if (myReader.hasNextLine()) {
-                data = myReader.nextLine();
-
-            }
-
             while (myReader.hasNextLine()) {
-                data = myReader.nextLine();
 
-                System.out.println(data);
+                var stringSequence = myReader.nextLine().split(" ");
+                res += getNextValue(Arrays.stream(stringSequence).map(Integer::parseInt).collect(Collectors.toList()));
             }
 
             myReader.close();
