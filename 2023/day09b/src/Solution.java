5794e3a1f2027f8286accc1541c1802012eb122b
Anrighe <assoonpc@gmail.com>
2024-01-03 20:17:17 +0100

    Solution day09b

--- 2023/day09b/src/Solution.java
+++ 2023/day09b/src/Solution.java
@@ -2,12 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import javafx.util.*;
 
 
 public class Solution {
@@ -28,73 +25,60 @@ public class Solution {
         return true;
     }
 
+    /**
+     * Calculates the next value in the sequence based on the given input sequence
+     *
+     * @param sequence the input sequence
+     * @return the next value in the sequence
+     */
     public static int getNextValue(List<Integer> sequence) {
         List<Integer> currentSequence = sequence;
 
         List<List<Integer>> allSequences = new ArrayList<List<Integer>>();
         allElementsAreZero(currentSequence);
 
-        int sum = 0;
-        boolean negative = true;
+        int first = 0;
 
         while (!allElementsAreZero(currentSequence)) {
 
             List<Integer> newSequence = new ArrayList<Integer>();
-            for (int i = 1; i < currentSequence.size(); i++) {
+            for (int i = 1; i < currentSequence.size(); i++)
                 newSequence.add(currentSequence.get(i) - currentSequence.get(i - 1));
-            }
+
             allSequences.add(newSequence);
             currentSequence = newSequence;
         }
 
-        // After all the sequences have been calculated, we need to iterate backwards and find from the sequence with only zeroes
-        // the first element of the first sequence by subtracting each time
-
-        System.out.print("Seqence: ");
-        for (var a : sequence) {
-            System.out.print(a + " ");
-        }
-        System.out.println();
-
-
-
+        // After all the sequences have been calculated, we need to iterate backwards and find from 
+        // the sequence with only zeroes the first element of the first sequence by subtracting each time
         for (int i = allSequences.size() - 1; i >= 0; i--) {
-            
             List<Integer> seq = allSequences.get(i);
-
-            System.out.print("Seqence: ");
-            for (var a : seq) {
-                System.out.print(a + " ");
-            }
-            System.out.println();
-            
-            
-            System.out.println(seq.get(0) + " - " + sum + " = " + (seq.get(0) - sum));
-            sum = seq.get(0) - sum;
+            first = seq.get(0) - first;
         }
 
-        // print allSequences
-        /*
-        System.out.println("All sequences: ");
-        for (List<Integer> seq : allSequences) {
-            for (int s : seq) {
-                System.out.print(s + " ");
-            }
-            System.out.println();
-        } */
-        System.out.println("-------------");
-
-
-
-        System.out.println("Sum: " + sum);
-        
-        return sum;
-
-
+        return sequence.get(0) - first;
     }
 
 
     /**
+     * 
+     * Based on the given sequences, calculates and sum all the next value in each sequence.
+     * Example: 
+     * Given the sequence "0 3 6 9 12 15" it must calculate first each differences between each element:
+     *                      3 3 3 3  3
+     * Then, if the differences are still not all zero, keep going:
+     *                       0 0 0 0
+     * When they are all zero, the next value in the sequence is the last element of the original sequence
+     *  plus the sum of the last differences sequence:
+     *   0 + 3 + 15 = 18
+     * 
+     * So the next value in the sequence is 18.
+     * 
+     * ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * 
+     * In the second part of the problem we need to calculate the first value of the sequence instead of the last,
+     *  then sum them all up and return the result.
+     * 
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
