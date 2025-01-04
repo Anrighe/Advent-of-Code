4f27c8fce813a673e25ac98f19b2dc384646486e
Anrighe <enrico.marras96@gmail.com>
2025-01-04 12:19:06 +0100

    wip

--- 2024/day10a/src/Main.java
+++ 2024/day10a/src/Main.java
@@ -2,20 +2,92 @@ import java.io.*;
 
 public class Main {
 
-    public static final String INPUT_FILE_LOCATION = "/home/marrase/Advent-of-Code/2024/day09b/input.txt";
+    public static final String INPUT_FILE_LOCATION = "/home/marrase/Advent-of-Code/2024/day10a/input.txt";
+
+    /**
+     * Returns the length of each line in the file if all lines have equal length.
+     * @return Length of lines in the file
+     * @throws FileNotFoundException If the file does not exist
+     * @throws IOException If an I/O error occurs
+     * @throws AssertionError If lines have different lengths
+     */
+    public static int getFileLineLengthIfAllAreEqual() throws FileNotFoundException, IOException, AssertionError {
+        File file = new File(INPUT_FILE_LOCATION);
+        BufferedReader br;
+        br = new BufferedReader(new FileReader(file));
+
+        String inputLine;
+        inputLine = br.readLine();
+        int firstLineLength = inputLine.length();
+
+        while ((inputLine = br.readLine()) != null)
+            assert firstLineLength == inputLine.length();
+
+        br.close();
+        return firstLineLength;
+    }
+
+    /**
+     * Counts the number of lines in the file.
+     * @return Number of lines in the file
+     * @throws FileNotFoundException If the file does not exist
+     * @throws IOException If an I/O error occurs
+     */
+    public static int getFileLineNumber() throws FileNotFoundException, IOException, AssertionError {
+        File file = new File(INPUT_FILE_LOCATION);
+        BufferedReader br;
+        br = new BufferedReader(new FileReader(file));
+
+        int lineNumber = 0;
+
+        while ((br.readLine()) != null)
+            lineNumber += 1;
+
+        br.close();
+        return lineNumber;
+    }
+
+    /**
+     * Prints a 2D character matrix to the console.
+     * @param mat The 2D character matrix
+     */
+    public static void print2DMatrix(char mat[][])
+    {
+        // Loop through all rows
+        for (int i = 0; i < mat.length; i++) {
+
+            // Loop through all elements of current row
+            for (int j = 0; j < mat[i].length; j++)
+                System.out.print(mat[i][j] + " ");
+                
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]) {
         File file = new File(INPUT_FILE_LOCATION);
 
         int result = 0;
         
-        try {     
+        try {
             BufferedReader br = new BufferedReader(new FileReader(file));
+
+            int col = getFileLineLengthIfAllAreEqual();
+            int row = getFileLineNumber();
+
+            assert col == row;
             
+            int matrix[][] = new int[row][col];
+
             String inputLine;
 
+            int currentRow = 0;
+            int currentCol = 0;
+            
             while ((inputLine = br.readLine()) != null) {
-                
+                for (char c : inputLine.toCharArray()) {
+                    matrix[]
+                }
             }
             br.close();
                 
@@ -26,6 +98,9 @@ public class Main {
             return;
         } catch (IOException e) {
             System.err.println(String.format("Error: IOException while reading file: %s", e.toString()));
+            return;
+        } catch (AssertionError e) {
+            System.err.println(String.format("Error: input file rows length not equal to input file column length: %s", e.toString()));
             return;
         }
     }
